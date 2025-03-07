e227a75a4e405689399f01390ac06e31f7f0630c
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-03-07 23:27:07 +0800

    [FEAT] Create a Cast model for movie credits (#23)
    ## Description
    
    - Create a Cast model for movie credits
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] This PR does not introduce a breaking change
    - [ ] Unit tests are added/updated (if applicable)
    - [] No error nor warning in the console

--- src/main/java/net/ow/movie/tmdb/model/movie/Cast.java
+++ src/main/java/net/ow/movie/tmdb/model/movie/Cast.java
@@ -1,3 +1,34 @@
 package net.ow.movie.tmdb.model.movie;
 
-public class Cast {}
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public class Cast {
+    private Boolean adult;
+
+    private Integer gender;
+
+    private Integer id;
+
+    @JsonAlias("known_for_department")
+    private String knownForDepartment;
+
+    private String name;
+
+    @JsonAlias("original_name")
+    private String originalName;
+
+    private Double popularity;
+
+    @JsonAlias("profile_path")
+    private String profilePath;
+
+    @JsonAlias("cast_id")
+    private Integer castId;
+
+    private String character;
+
+    @JsonAlias("credit_id")
+    private String creditId;
+
+    private Integer order;
+}
