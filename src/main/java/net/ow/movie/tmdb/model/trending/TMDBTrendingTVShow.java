c34a7f0bd7daf7a4bf51e2d8376a2f23193d10ae
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-04-04 14:13:52 +0800

    [FEAT] Integrate with TMDB to fetch trending tv shows (#44)
    ## Description
    
    - Integrate with TMDB to fetch trending tv shows
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] This PR does not introduce a breaking change
    - [ ] Unit tests are added/updated (if applicable)
    - [x] No error nor warning in the console

---
+++ src/main/java/net/ow/movie/tmdb/model/trending/TMDBTrendingTVShow.java
@@ -0,0 +1,63 @@
+package net.ow.movie.tmdb.model.trending;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import net.ow.movie.tmdb.deserializer.ImagePathDeserializer;
+import net.ow.movie.tmdb.serializer.ImagePathSerializer;
+import net.ow.shared.jsonutils.deserializer.DateInstantDeserializer;
+import net.ow.shared.jsonutils.serializer.DateInstantSerializer;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class TMDBTrendingTVShow extends TMDBTrending {
+    private Boolean adult;
+
+    @JsonAlias("backdrop_path")
+    @JsonSerialize(using = ImagePathSerializer.class)
+    @JsonDeserialize(using = ImagePathDeserializer.class)
+    private String backdropPath;
+
+    private String name;
+
+    @JsonAlias("original_language")
+    private String originalLanguage;
+
+    @JsonAlias("original_name")
+    private String originalName;
+
+    private String overview;
+
+    @JsonAlias("poster_path")
+    @JsonSerialize(using = ImagePathSerializer.class)
+    @JsonDeserialize(using = ImagePathDeserializer.class)
+    private String posterPath;
+
+    @JsonAlias("genre_ids")
+    private Integer[] genreIds;
+
+    private BigDecimal popularity;
+
+    @JsonAlias("first_air_date")
+    @JsonSerialize(using = DateInstantSerializer.class)
+    @JsonDeserialize(using = DateInstantDeserializer.class)
+    private Instant firstAirDate;
+
+    @JsonAlias("vote_average")
+    private BigDecimal voteAverage;
+
+    @JsonAlias("vote_count")
+    private Integer voteCount;
+
+    @JsonAlias("origin_country")
+    private List<String> originCountry;
+}
