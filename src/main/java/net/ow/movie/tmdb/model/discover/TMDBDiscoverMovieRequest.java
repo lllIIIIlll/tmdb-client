8af50f2f729a8b9ce1758dfed3a1c2f8f75e58ea
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-04-04 15:14:24 +0800

    [RFT] Correct data type and rename the abbreviation (#46)
    ## Description
    
    - Correct data type
    - Rename the abbreviation such as lte and gte to greater then and less
    then
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] This PR does not introduce a breaking change
    - [ ] Unit tests are added/updated (if applicable)
    - [x] No error nor warning in the console

--- src/main/java/net/ow/movie/tmdb/model/discover/TMDBDiscoverMovieRequest.java
+++ src/main/java/net/ow/movie/tmdb/model/discover/TMDBDiscoverMovieRequest.java
@@ -1,5 +1,11 @@
 package net.ow.movie.tmdb.model.discover;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
+import java.math.BigDecimal;
+import java.time.Instant;
 import lombok.Builder;
 import lombok.Data;
 import net.ow.shared.common.server.annotation.QueryParameter;
@@ -7,6 +13,18 @@ import net.ow.shared.common.server.annotation.QueryParameter;
 @Data
 @Builder
 public class TMDBDiscoverMovieRequest {
+    @QueryParameter(name = "certification")
+    private String certification;
+
+    @QueryParameter(name = "certification.gte")
+    private String certificationGreaterThen;
+
+    @QueryParameter(name = "certification.lte")
+    private String certificationLessThen;
+
+    @QueryParameter(name = "certification_country")
+    private String certificationCountry;
+
     @QueryParameter(name = "include_adult")
     private Boolean includeAdult;
 
@@ -17,103 +35,99 @@ public class TMDBDiscoverMovieRequest {
 
     private Integer page;
 
+    @QueryParameter(name = "primary_release_year")
+    private Integer primaryReleaseYear;
+
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = InstantDeserializer.class)
+    @QueryParameter(name = "primary_release_date.gte")
+    private Instant primaryReleaseDateGreaterThen;
+
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = InstantDeserializer.class)
+    @QueryParameter(name = "primary_release_date.lte")
+    private Instant primaryReleaseDateLessThen;
+
+    private String region;
+
+    @QueryParameter(name = "release_date.gte")
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = InstantDeserializer.class)
+    private Instant releaseDateGreaterThen;
+
+    @QueryParameter(name = "release_date.lte")
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = InstantDeserializer.class)
+    private Instant releaseDateLessThen;
+
     @QueryParameter(name = "sort_by")
     private String sortBy;
 
-    private String region;
+    @QueryParameter(name = "vote_average.gte")
+    private BigDecimal voteAverageGreaterThen;
+
+    @QueryParameter(name = "vote_average.lte")
+    private BigDecimal voteAverageLessThen;
+
+    @QueryParameter(name = "vote_count.gte")
+    private Integer voteCountGreaterThen;
+
+    @QueryParameter(name = "vote_count.lte")
+    private Integer voteCountLessThen;
 
     @QueryParameter(name = "watch_region")
     private String watchRegion;
 
-    @QueryParameter(name = "certification_country")
-    private String certificationCountry;
-
-    @QueryParameter(name = "certification")
-    private String certification;
-
-    @QueryParameter(name = "certification.gte")
-    private String certificationGte;
-
-    @QueryParameter(name = "certification.lte")
-    private String certificationLte;
-
-    @QueryParameter(name = "primary_release_date.gte")
-    private String primaryReleaseDateGte;
-
-    @QueryParameter(name = "primary_release_date.lte")
-    private String primaryReleaseDateLte;
-
-    @QueryParameter(name = "primary_release_year")
-    private Integer primaryReleaseYear;
-
-    @QueryParameter(name = "release_date.gte")
-    private String releaseDateGte;
-
-    @QueryParameter(name = "release_date.lte")
-    private String releaseDateLte;
-
-    private Integer year;
-
-    @QueryParameter(name = "vote_average.gte")
-    private Float voteAverageGte;
-
-    @QueryParameter(name = "vote_average.lte")
-    private Float voteAverageLte;
-
-    @QueryParameter(name = "vote_count.gte")
-    private Integer voteCountGte;
-
-    @QueryParameter(name = "vote_count.lte")
-    private Integer voteCountLte;
-
-    @QueryParameter(name = "with_runtime.gte")
-    private Integer withRuntimeGte;
-
-    @QueryParameter(name = "with_runtime.lte")
-    private Integer withRuntimeLte;
-
-    @QueryParameter(name = "with_release_type")
-    private Integer withReleaseType;
-
-    @QueryParameter(name = "with_original_language")
-    private String withOriginalLanguage;
-
-    @QueryParameter(name = "with_original_country")
-    private String withOriginalCountry;
-
     @QueryParameter(name = "with_cast")
     private String withCast;
-
-    @QueryParameter(name = "with_crew")
-    private String withCrew;
-
-    @QueryParameter(name = "with_people")
-    private String withPeople;
-
-    @QueryParameter(name = "with_genres")
-    private String withGenres;
-
-    @QueryParameter(name = "without_genres")
-    private String withoutGenres;
-
-    @QueryParameter(name = "with_keywords")
-    private String withKeywords;
-
-    @QueryParameter(name = "without_keywords")
-    private String withoutKeywords;
 
     @QueryParameter(name = "with_companies")
     private String withCompanies;
 
-    @QueryParameter(name = "without_companies")
-    private String withoutCompanies;
+    @QueryParameter(name = "with_crew")
+    private String withCrew;
+
+    @QueryParameter(name = "with_genres")
+    private String withGenres;
+
+    @QueryParameter(name = "with_keywords")
+    private String withKeywords;
+
+    @QueryParameter(name = "with_origin_country")
+    private String withOriginCountry;
+
+    @QueryParameter(name = "with_original_language")
+    private String withOriginalLanguage;
+
+    @QueryParameter(name = "with_people")
+    private String withPeople;
+
+    @QueryParameter(name = "with_release_type")
+    private Integer withReleaseType;
+
+    @QueryParameter(name = "with_runtime.gte")
+    private Integer withRuntimeGreaterThen;
+
+    @QueryParameter(name = "with_runtime.lte")
+    private Integer withRuntimeLessThen;
+
+    @QueryParameter(name = "with_watch_monetization_types")
+    private String withWatchMonetizationTypes;
 
     @QueryParameter(name = "with_watch_providers")
     private String withWatchProviders;
 
+    @QueryParameter(name = "without_companies")
+    private String withoutCompanies;
+
+    @QueryParameter(name = "without_genres")
+    private String withoutGenres;
+
+    @QueryParameter(name = "without_keywords")
+    private String withoutKeywords;
+
     @QueryParameter(name = "without_watch_providers")
     private String withoutWatchProviders;
 
-    @QueryParameter(name = "with_watch_monetization_types")
-    private String withWatchMonetizationTypes;
+    private Integer year;
 }
