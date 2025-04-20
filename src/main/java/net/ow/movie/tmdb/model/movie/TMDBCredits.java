a61e8d3a6e6c85c2fc83dd6f8ab0c766f310b990
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-04-20 20:06:35 +0800

    [RFT] Move TMDBCast and TMDBCrew to person package (#51)
    ## Description
    
    - Move TMDBCast and TMDBCrew to person package
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] This PR does not introduce a breaking change
    - [ ] Unit tests are added/updated (if applicable)
    - [x] No error nor warning in the console

--- src/main/java/net/ow/movie/tmdb/model/movie/TMDBCredits.java
+++ src/main/java/net/ow/movie/tmdb/model/movie/TMDBCredits.java
@@ -4,6 +4,8 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.ow.movie.tmdb.model.person.TMDBCast;
+import net.ow.movie.tmdb.model.person.TMDBCrew;
 
 @Data
 @NoArgsConstructor
--- src/main/java/net/ow/movie/tmdb/model/person/TMDBCast.java
+++ src/main/java/net/ow/movie/tmdb/model/person/TMDBCast.java
@@ -1,4 +1,4 @@
-package net.ow.movie.tmdb.model.movie;
+package net.ow.movie.tmdb.model.person;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
