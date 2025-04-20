e9e8d03e6db7e36f0544c2f69f3888e1a8b444c2
Oliver <hohang210@gmail.com>
2025-04-21 00:37:58 +0800

    [FEAT] Add credtis and recommendations attribute to movie

---
+++ src/main/java/net/ow/movie/tmdb/constant/TMDBJob.java
@@ -0,0 +1,10 @@
+package net.ow.movie.tmdb.constant;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class TMDBJob {
+    public static final String DIRECTOR = "Director";
+
+    public static final String EDITOR = "Editor";
+}
